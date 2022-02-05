770cfc9ecbc5b52a3254277ab379b32ae019065b
tobias <tobiasmesa030303@gmail.com>
2022-02-05 20:44:09 -0300

    idea en script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+//Agregar boton de mas 
+//Vincularle la funcion de agregar tareas
+//cunado se agrega una tarea se genera un nuevo div (con un titulo descripcion y dos botones)
+//fucion borrar div 
+//funcione editar div
+//
